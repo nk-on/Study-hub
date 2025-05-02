02b9e3b77fdb58cf5dd1b43e460482cbe40841a2
nk-on <2nkoz@proton.me>
2025-05-01 21:58:58 -0700

    Formating code

--- app/_heading/Heading.tsx
+++ app/_heading/Heading.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { subjects } from '../Subjects'
+import React from "react";
+import { subjects } from "../Subjects";
+import Link from "next/link";
 function Heading() {
   return (
-    <nav className='w-[100%] py-[40px] shadow-xl'>
-        <ul className='flex justify-around'>
-            {
-              subjects.map(element => <li key={element.subjectId}  className='cursor-pointer'>{element.subjectName}</li>)
-            }
-            <li>Log in</li>
-            <li>Register</li>
-        </ul>
+    <nav className="w-[100%] py-[40px] shadow-xl">
+      <ul className="flex justify-around">
+        {subjects.map((element) => (
+          <li key={element.subjectId} className="cursor-pointer">
+            <Link href={"subject"}>{element.subjectName}</Link>
+          </li>
+        ))}
+        <li>Log in</li>
+        <li>Register</li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Heading
+export default Heading;
