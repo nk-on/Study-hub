ed2754c56f3e5842591444c0f08972abccb213f4
nk-on <2nkoz@proton.me>
2025-05-01 21:28:46 -0700

    Feat:building basic skeleton of project

---
+++ app/_heading/Heading.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <nav className='w-[100%] py-[40px] shadow-xl'>
+        <ul className='flex justify-around'>
+            <li>Math</li>
+            <li>Physics</li>
+            <li>Biology</li>
+            <li>Chemistry</li>
+            <li>Login</li>
+            <li>Register</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Heading
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Heading from "./_heading/Heading";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Heading />
         {children}
       </body>
     </html>
