e5b09af3c0e3b9c9349a6814ece7e7a894847902
nk-on <2nkoz@proton.me>
2025-05-05 07:27:09 -0700

    Feat:implementing feature which lets user to open youtube video on selected topic on new tab

--- app/(site)/subject/[id]/page.tsx
+++ app/(site)/subject/[id]/page.tsx
@@ -1,17 +1,22 @@
-import { subjects } from "@/app/Subjects"
-function Subject({params}:{params:{id:string}}) {
-  const id = params.id
-  const subject = subjects[Number(id)]
+import { subjects } from "@/app/Subjects";
+function Subject({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const subject = subjects[Number(id)];
   return (
     <div className="flex flex-col justify-center items-center gap-[20px] mt-[10%]">
-       <h1>{subject.subjectName}</h1>
-       <ul className="w-[100%] flex flex-col justify-center items-center gap-[30px]">
-        {
-          subject.topics.map((topicName,index) => <li key={index} className="w-[50%] h-[30px] bg-[#acafc0] flex justify-center cursor-pointer">{topicName.topicName}</li>)
-        }
-       </ul>
+      <h1>{subject.subjectName}</h1>
+      <ul className="w-[100%] flex flex-col justify-center items-center gap-[30px]">
+        {subject.topics.map((topicName, index) => (
+          <li
+            key={index}
+            className="w-[50%] h-[30px] bg-[#acafc0] flex justify-center cursor-pointer"
+          >
+            <a href = {`${topicName.youtubeLink}`} target="_blank">{topicName.topicName}</a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Subject
+export default Subject;
